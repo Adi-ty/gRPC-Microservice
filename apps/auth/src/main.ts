0d7ba26cbcf87abdf0f955701747bb5a8b119d77
Aditya <adityasinghboss1234@gmail.com>
2023-09-02 06:27:15 +0530

    feat: ✨Implement Service Gateway

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
-import { AUTH } from '@app/common';
+import { AUTH_PACKAGE_NAME } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -11,7 +11,7 @@ async function bootstrap() {
       transport: Transport.GRPC,
       options: {
         protoPath: join(__dirname, '../auth.proto'),
-        package: AUTH,
+        package: AUTH_PACKAGE_NAME,
       },
     },
   );
